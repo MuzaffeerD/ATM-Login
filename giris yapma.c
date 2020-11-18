6ef0d9e2417dbf287f5cad5e7266dc36211638a6
Muzaffer Deliali <46688496+MuzaffeerD@users.noreply.github.com>
2020-11-18 22:08:11 +0300

    Add files via upload

---
+++ giris yapma.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    static const char PSWRD[]="1234";
+    char p[6];
+    int n=1, y;
+
+    printf("Hos geldiniz");
+
+
+    do{
+        printf("\n\nOgrenci_ID:Elif");
+        fflush(stdout);
+
+        printf("\nSifre:");
+        scanf("%s", &p);
+        fflush(stdout);
+
+       
+        y=strcmp(p, PSWRD);
+
+        if(y==0){
+            printf("\nGiris Basarili");
+            return 0;
+        }else {
+            printf("Yanlis Sifre, tekrar deneyiniz", 3-n);
+            getchar();
+            n++;}
+
+        if(n>3){
+            printf("\nHesabiniz bloke oldu");
+        }
+
+    }while (n<=3);
+	return 0;
+}
