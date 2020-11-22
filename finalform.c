c0854b7afbdf635f98509ae6264a2d9c369cd34b
Muzaffer Deliali <46688496+MuzaffeerD@users.noreply.github.com>
2020-11-22 16:49:30 +0300

    Add files via upload

---
+++ finalform.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+
+int main()
+    {
+        static const char PSWRD[]="1234";
+        char p[6];
+        int n=3, y;
+    	int islem;
+		int bakiye =100;
+		int tutar;
+        printf("Welcome");
+    
+    
+        do{
+            printf("\n\nStudent_ID:Elif");
+            fflush(stdout);
+    
+            printf("\nPassword:");
+            scanf("%s", p);
+            fflush(stdout);
+    
+           
+            y=strcmp(p, PSWRD);
+    
+            if(y==0){
+                printf("\nSucessfull Login\n"); //succesfull login
+	printf("Islemler\nPara yatirmak icin 1 \nPara cekmek icin 2\nHesap bakiyenizi gormek icin 3\nCikmak icin 4 girimiz");
+	do{
+
+	printf("\n\nIslem Seciniz:");
+	scanf("%d",&islem);{
+
+	switch(islem){
+
+	case 1:
+		printf("Bakiyeniz %d \n",bakiye);
+		printf("\nYatirilacak Tutar:");
+		scanf("%d",&tutar);
+		bakiye += tutar;
+		printf("Bakiyeniz: %d",bakiye);
+		break;
+		
+	case 2:
+		printf("Bakiyeniz %d \n",bakiye);
+		printf("\nCekilecek Tutar:");
+		scanf("%d",&tutar);
+		if (tutar > bakiye){
+			printf("Bakiye yetersiz\n");
+		}
+		bakiye -= tutar;
+		printf("Bakiyeniz: %d",bakiye);
+		break;
+
+	case 3:
+		printf("Bakiyeniz: %d",bakiye);
+		break;	
+		
+	case 4:
+		printf("Cikis Yapiliyor");
+		return 0;
+		
+	default:
+		printf("Hatali Islem");
+		return 0;
+	}			
+	}	} while (islem);
+	return 0;
+}
+            else{{
+			{
+                n--;
+                    printf("\nWrong password, try again: "); //wrong password try again
+                    printf("\nRemaining attempts  ");
+                    printf("%d\n", n);
+                    getchar();
+            }
+    
+            if(n<1){
+                printf("\nYour account has been blocked\n"); 
+                    return 0;
+            }
+        }while (n>0);
+                            return 0;
